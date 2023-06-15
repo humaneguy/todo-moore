37f0801cf7ad1dedf22dd8015602c67ec4d84bbb
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2023-06-15 05:15:01 -0700

    protect using auth middleware

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 
-Route::resource('todos', TodoController::class);
+Route::resource('todos', TodoController::class)->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
